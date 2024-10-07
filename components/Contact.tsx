aab085af3ac79cb714f25fddae8410665b8e6534
UtkuBoraEfil <utqbora@hotmail.com>
2024-10-07 10:48:23 +0300

    footer done

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import arrow from "@/public/images/arrow-up.png";
 import { Instagram } from "lucide-react";
 import whatsapp from "@/public/images/whatsapp.png";
 import { Volleyball1 } from "./ui/volleyball1";
@@ -27,7 +28,14 @@ export function Contact() {
         </div>
       </div>
       <div className="flex justify-between items-end">
-        <h1 className=" text-[156px] leading-none">REVESPORT</h1>
+        <div className="relative">
+          <h1 className=" text-[156px] leading-none">REVESPORT</h1>
+          <div className="absolute w-full -top-10 flex justify-center">
+          <button className="mx-auto  arrow-down px-4 py-4">
+            <Image src={arrow} alt="" />
+          </button>
+          </div>
+        </div>
         <div className="relative">
           <p className=" font-roboto opacity-90 leading-none">
             {" "}
