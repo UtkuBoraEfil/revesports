a9d54c03fcf7da4cc33682ddf1114a0fbe35af91
UtkuBoraEfil <utqbora@hotmail.com>
2024-10-19 00:33:54 +0300

    tracing-beam

--- app/blog/[slugs]/page.tsx
+++ app/blog/[slugs]/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+import React from "react";
+import { twMerge } from "tailwind-merge";
+import { TracingBeam } from "@/components/ui/tracing-beam";
 import { blogs } from "@/lib/blogs";
 import { Navbar } from "@/components/ui/navbar";
 import Image from "next/image";
@@ -8,16 +12,16 @@ export default function BlogPart({ params }: { params: any }) {
   return (
     <section className="bg-default-black text-white">
       <Navbar />
-      <div className="xl:px-28 md:px-14 px-6 py-14 font-roboto">
-        <h1 className=" text-4xl md:text-8xl text-center font-impact">{blog.title}</h1>
-        <div className="w-full flex justify-between pt-12 items-center">
+      <TracingBeam className=" px-6 py-14 font-roboto">
+        <h1 className=" text-5xl md:text-6xl lg:text-8xl text-center font-impact">{blog.title}</h1>
+        <div className="w-full flex justify-between pt-6 items-center">
           <div className="flex gap-4 items-center">
             <Image
               src={"/images/academic.jpg"}
               width={30}
               height={30}
               alt={"author"}
-              className="rounded-full my-3"
+              className="rounded-full"
             />
             <p className="md:text-lg">{blog.author}</p>
           </div>
@@ -28,10 +32,10 @@ export default function BlogPart({ params }: { params: any }) {
           alt={"image"}
           width={1000}
           height={1000}
-          className="w-full h-96 object-cover object-center rounded-md my-12"
+          className="w-full h-96 object-cover object-center rounded-md my-6"
         />
         <p className="md:text-xl leading-7">{blog.text}</p>
-      </div>
+      </TracingBeam>
     </section>
   );
 }
